1f39ad4142ffefc122173ff7dde6ee4b992a7959
kinjal <kinjaldoesai@gmail.com>
2025-04-21 14:44:11 +0200

    Added Port again

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from app import app  # or however you're importing your Flask app
-import os
+from app import create_app  # Import the create_app function from app/__init__.py
+
+app = create_app()  # Call the function to create the Flask app instance
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
 
 
